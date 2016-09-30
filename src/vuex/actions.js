8d4f56e2a27c175e75e653ace628da0a1e2f256e
cctat <zcc0810@hotmail.com>
2016-09-30 15:26:02 +0800

    add

---
+++ src/vuex/actions.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import marked from 'marked';
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false,
+  highlight: function (code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
+
+
+export const renderHtml = ({dispatch},e)=>{
+    var _renderHtml = marked(e.target.value) 
+    return dispatch('MARKDOWN_SUCCESS',e.target.value,_renderHtml)
+}
